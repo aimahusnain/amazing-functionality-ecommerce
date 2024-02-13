974a9730c27e80d6871bad10cfc0e3c1275e2f44
Muhammad Husnain <aimahusnain@gmail.com>
2024-02-13 19:34:30 +0500

    my one

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,8 @@
 // src/app/api/auth/[...nextauth]/route.ts
 
-import { NextApiRequest, NextApiResponse } from 'next';
-import NextAuth from 'next-auth';
+import { authOptions } from "@/lib/auth"
+import NextAuth from "next-auth"
 
-import { authOptions } from '@/lib/auth'; // Adjust the path as needed
+const handler = NextAuth(authOptions)
 
-// Ensure the exported default function is named `handler`
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  // Pass the request, response, and authentication options to NextAuth
-  await NextAuth(req, res, authOptions);
-}
+export { handler as GET, handler as POST }
